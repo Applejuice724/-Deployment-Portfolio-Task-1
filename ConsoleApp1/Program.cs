a25978fa84e90390268ebcba0d4c93931c6ab2e8
Remy (AppleBabble) <jeremy.grassi@gmail.com>
2026-08-17 12:04:12 +1000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deployment Activity 1: Pass Task Completed!");
+            Console.WriteLine("Hello world! This is a simple console application.");
+            Console.WriteLine("Please enter a value:");
+            string value1 = Console.ReadLine();
+            Console.WriteLine($"You entered: {value1}");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
