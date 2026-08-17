953ca7ce4ef6758aaa68461560448ae2de7bfa68
Remy (AppleBabble) <jeremy.grassi@gmail.com>
2026-08-17 12:54:47 +1000

    Add user input, JSON serialization, and TimeSpan humanize
    Added Newtonsoft.Json and Humanizer references. Prompted for user input, serialized it to JSON, and displayed the result. Demonstrated TimeSpan humanization and improved console output for interaction. This is to acheive distinction for the tasks with added libraries

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using Humanizer;
 namespace SampleApp
 {
     class Program
@@ -7,11 +9,23 @@ namespace SampleApp
         {
             Console.WriteLine("Deployment Activity 1: Pass Task Completed!");
             Console.WriteLine("Hello world! This is a simple console application.");
+
             Console.WriteLine("Please enter a value:");
             string value1 = Console.ReadLine();
+
             Console.WriteLine($"You entered: {value1}");
+            string json = JsonConvert.SerializeObject(value1);
+
+            Console.WriteLine($"Serialized JSON: {json}");
+
+            TimeSpan duration = TimeSpan.FromMinutes(125);
+
+            Console.WriteLine($"Duration: {duration}");
+            Console.WriteLine($"Humanized duration: {duration.Humanize()}");
+
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
     }
 }
+
